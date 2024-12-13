ba6d73562a1fb518913ae0e8a805bcdb08dcc26a
0bscurity <andrew@isifederal.com>
2024-12-13 10:11:12 -0500

    Add dynamic county dropdown based on state selection
    Introduced a new `FetchCountiesView` to dynamically fetch and render county options when a state is selected in the state calculator. Updated templates and URLs to support this functionality and improve the user experience with more responsive data loading.

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from pages.views.federal_calculator_views import HomePageView, FederalCalculatorView, FederalTaxCalculateView
-from pages.views.state_calculator_views import StateCalculatorView, StateTaxCalculateView
+from pages.views.state_calculator_views import StateCalculatorView, StateTaxCalculateView, FetchCountiesView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -10,4 +10,5 @@ urlpatterns = [
 
     path('state-calculator/', StateCalculatorView.as_view(), name='state_calculator'),
     path('state-calculator/calculate/', StateTaxCalculateView.as_view(), name='state_tax_calculate'),
+    path('state-calculator/fetch-counties/', FetchCountiesView.as_view(), name='fetch_counties'),
 ]
